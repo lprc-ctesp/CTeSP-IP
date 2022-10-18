3eb1110e44c8a71b8f9c2b968b2403eeac27961e
lprc-ctesp <luizpaulo.carvalho@my.istec.pt>
2022-10-18 14:42:31 +0100

    Projects rename

---
+++ Aula_04_Exercicios/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_04_Exercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
