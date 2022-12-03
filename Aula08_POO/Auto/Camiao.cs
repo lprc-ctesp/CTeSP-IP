0bef03bbc50fcb42ab3acdad5a2ae34562234892
lprc-ctesp <luizpaulo.carvalho@my.istec.pt>
2022-12-03 11:29:04 +0000

    Commit Aula 08 POO

---
+++ Aula08_POO/Auto/Camiao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula08_POO.Auto
+{
+    internal class Camiao : Veiculo
+    {
+        public Camiao(int modelo, double preco) : base(modelo, preco) { }
+
+        public override void Iniciar()
+        {
+            Console.WriteLine("O Camião iniciou....");
+        }
+
+        public override void Acelerar()
+        {
+            Console.WriteLine("O camião acelerou....");
+        }
+
+        public override string ToString()
+        {
+            return "Camião: " + base.ToString();
+        }
+    }
+}
---
+++ Aula08_POO/Auto/Veiculo.cs
@@ -0,0 +1,48 @@
+﻿namespace Aula08_POO.Auto
+{
+    abstract class Veiculo
+    {
+        private int matricula;
+        private int modelo;
+        private double preco;
+        private static int numeroSerie = 0;
+
+        public int Matricula
+        {
+            get { return matricula; }
+        }
+
+        public int Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+
+        public double Preco
+        {
+            get { return preco; }
+            set { preco = value; }
+        }
+
+        public static int NumeroSerie
+        {
+            get { return numeroSerie; }
+        }
+
+        public Veiculo(int modelo, double preco)
+        {
+            numeroSerie++;
+            matricula = numeroSerie;
+            this.modelo = modelo;
+            this.preco = preco;
+        }
+
+        public abstract void Iniciar();
+        public abstract void Acelerar();
+
+        public override string ToString()
+        {
+            return "Matrícula: " + matricula + " Modelo: " + modelo + " Preço: " + preco;
+        }
+    }
+}
